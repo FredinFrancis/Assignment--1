0cfdfec4741746dc21b147c9892001f78494fdb3
FredinFrancis <fredinfrancis010@gmail.com>
2021-10-02 07:22:11 +0530

    init

--- Fredin_Francis_8755328_Assignment1.py
+++ Fredin_Francis_8755328_Assignment1.py
@@ -15,10 +15,10 @@ select=int(input("Please enter your selection:- "))
 # Execution as per selection
 
 if (select==1):
-    print("You have choosen Polo Shirts!")
+    print("You have chosen Polo Shirts!")
     print("\n")
     col1= str(input("Please enter your desired colour:- "))
-    qty1= int(input("Please enter your required quanitity:- "))
+    qty1= int(input("Please enter your required quantity:- "))
     rate=9.99
     hstrate=1.13
     netprice1=rate*qty1
@@ -31,7 +31,7 @@ if (select==1):
     print(" Confirm your order !")
     print(" Type of Shirt- Polo Shirt")
     print(" Colour- "+ col1)
-    print(" Quanitity- "+ str(qty1))
+    print(" Quantity- "+ str(qty1))
     print(" Taxable Value = %.2f" % (netprice1))
     print(" HST @ 13 = %.2f" % (hst))
     print(" Total Price = $ %.2f" % (total))
@@ -49,7 +49,7 @@ if (select==1):
         print(" Order Not Confirmed !")
 
 elif(select==2):
-    print("You have choosen T-Shirts!")
+    print("You have chosen T-Shirts!")
     print("\n")
     col1= str(input("Please enter your desired colour:- "))
     qty1= int(input("Please enter your required quanitity:- "))
@@ -65,14 +65,14 @@ elif(select==2):
     print(" Confirm your order !")
     print(" Type of Shirt- T-Shirt")
     print(" Colour- "+ col1)
-    print(" Quanitity- "+ str(qty1))
+    print(" Quantity- "+ str(qty1))
     print(" Taxable Value = %.2f" % (netprice1))
     print(" HST @ 13 = %.2f" % (hst))
     print(" Total Price = $ %.2f" % (total))
 
     #Confirmation Page
     
-    confirm=int(input("Enter 1 to confirm / Enter 0 to go back to main menu :- "))
+    confirm=int(input("Enter 1 to confirm :- "))
     
     if(confirm==1):
         print("\n")
